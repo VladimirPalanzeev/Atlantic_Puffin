3b830bbc2d492fb175c2cbf36a970e6c9ae0c8f9
VladimirPalanzeev <palanzey@gmail.com>
2022-03-29 20:04:06 +0500

    [upd] idea in gitignore

---
+++ fratercula.main.py
@@ -0,0 +1,6 @@
+from fratercula import Fratercula
+import uuid
+
+a = Fratercula("Сокол", "Fratercula arctica arctica", 200, 15, 2019, "Арктика", uuid.id)
+
+print(a.name)
---
+++ fratercula.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+class Fratercula:
+    """Основные характеристики тупиков"""
+    def __init__(self, name, subspecies ,weight, growth, year_of_birth, habitat, id):
+        self.name = name                          # Кличка
+        self.subspecies = subspecies              # Подвид
+        self.weight = weight                      # Вес
+        self.growth = growth                      # Рост
+        self.year_of_birth = year_of_birth        # Год рождения
+        self.habitat = habitat                    # Среда обитания
+        self.id = id                         # Уникальный идентификатор
+
+    def age(self, year_of_birth):
+        date = datetime.now()
+        return date.year - year_of_birth
